24bf649e1eff03a82f67a27a8a831dc3005e9787
xuchenzhuo <xuchenzhuo@wisdomgarden.com>
2022-02-28 11:10:39 +0800

    new semester, format codes coded during winter vication

--- machineLearning/dataPretreatment/normalized.py
+++ machineLearning/dataPretreatment/normalized.py
@@ -1,6 +1,8 @@
 import numpy as np
 import os
 import csv
+
+
 # 数据归一化方法：
 # 1.效益型 越大越好-----------	> 大的大
 # 2.成本型 越小越好-----------	> 小的大
@@ -8,46 +10,63 @@ import csv
 # 4.偏离型 约远离某个值越好----->
 # 5.区间型 越靠近某个区间越好---	>
 
-#传入numpy.array
-#传入每个属性对应的归一化方式 一维向量
-def normalize(arr,method,standard=None):
-	colomnCount = len(arr[0])
-	rowCount = len(arr)
 
-	if (standard != None) and (len(standard) != colomnCount):
-		return "error: wrong standard length"
+# 传入numpy.array
+# 传入每个属性对应的归一化方式 一维向量
+def normalize(arr, method, standard=None):
+    columnCount = len(arr[0])
+    rowCount = len(arr)
 
-	for colomn in range(colomnCount):
-		currentColomn = arr[:,colomn]
-		maxOfColomn = np.max(currentColomn)
-		minOfColomn = np.min(currentColomn)
+    if (standard is not None) and (len(standard) != columnCount):
+        return "error: wrong standard length"
 
-		for row in range(rowCount):
-			if method[colomn] == 1:
-				arr[row,colomn] = (arr[row,colomn] - minOfColomn)/(maxOfColomn - minOfColomn)
-			elif method[colomn] == 2:
-				arr[row,colomn] = (maxOfColomn - arr[row,colomn])/(maxOfColomn - minOfColomn)
-			elif method[colomn] == 3 and standard:
-				arr[row,colomn] = (abs(standard[colomn] - arr[row,colomn]))/(minOfColomn - maxOfColomn) + 1
-			elif method[colomn] == 4 and standard:
-				arr[row,colomn] = (abs(arr[row,colomn]-standard[colomn])/(maxOfColomn - minOfColomn))
-			elif method[colomn] == 5:
-				pass
-			else:
-				return "error: wrong normalize method input"
-	return arr
+    for column in range(columnCount):
+        currentColumn = arr[:, column]
+        maxOfColumn = np.max(currentColumn)
+        minOfColumn = np.min(currentColumn)
+
+        for row in range(rowCount):
+            if method[column] == 1:
+                arr[row, column] = (arr[row, column] - minOfColumn) / (maxOfColumn - minOfColumn)
+            elif method[column] == 2:
+                arr[row, column] = (maxOfColumn - arr[row, column]) / (maxOfColumn - minOfColumn)
+            elif method[column] == 3 and standard:
+                arr[row, column] = (abs(standard[column] - arr[row, column])) / (minOfColumn - maxOfColumn) + 1
+            elif method[column] == 4 and standard:
+                arr[row, column] = (abs(arr[row, column] - standard[column]) / (maxOfColumn - minOfColumn))
+            elif method[column] == 5:
+                pass
+            else:
+                return "error: wrong normalize method input"
+    return arr
+
 
 # put datasets which the path ordered together, and ends with '.dataType'
-def putDatasTogether(path,dataType):
-	files = os.listdir(path)
-	featureSet = set()
-	for file in files:
-		if not ( file.endswith('.'+dataType) and file.startswith('drill') ):
-			files.pop(files.index(file))
-	for file in files:
-		with open(path + '/' + file, encoding="utf-8") as f:
-			csvFile = list(csv.reader(f))
-			features = csvFile[0]
-		for feature in features:
-			featureSet.add(feature.split(' ')[0])
-	return sorted(featureSet)
+def putDatasTogether(path, dataType):
+    files = os.listdir(path)
+    featureSet = set()
+    for file in files:
+        if not (file.endswith('.' + dataType) and file.startswith('drill')):
+            files.pop(files.index(file))
+    files.pop(files.index('README.md'))
+    with open(path + "/attributes.py", encoding="utf-8") as attr:
+        existAttr = len(attr.readlines())
+    for file in files:
+        with open(path + '/' + file, encoding="utf-8-sig") as f:
+            csvFile = list(csv.reader(f))
+            features = csvFile[0]
+        for feature in features:
+            feature = feature.split(' ')
+            try:
+                feature[1] = "\"" + feature[1] + "\""
+            except:
+                feature.append("\"\"")
+            finally:
+                feature[-1] = feature[-1] + '\n'
+                featureSet.add(' = '.join(feature))
+    featureSet = list(featureSet)
+    featureSet.pop(featureSet.index(' = ""\n'))
+    if len(featureSet) != existAttr:
+        with open(path + "/attributes.py", "wt", encoding="utf-8-sig") as attr:
+            attr.writelines(sorted(featureSet))
+    return sorted(featureSet)
