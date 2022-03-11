e4108da47c47228802997f5eb197a1e5fa1c4850
xuchenzhuo <xu_3_1415926@sina.com>
2022-03-11 12:52:18 +0800

    put datas in a table

--- machineLearning/dataPretreatment/normalized.py
+++ machineLearning/dataPretreatment/normalized.py
@@ -8,8 +8,7 @@ import csv
 # 2.成本型 越小越好-----------	> 小的大
 # 3.固定型 越接近某个值越好	---->
 # 4.偏离型 约远离某个值越好----->
-# 5.区间型 越靠近某个区间越好---	>
-
+# 5.区间型 越靠近某个区间越好--->
 
 # 传入numpy.array
 # 传入每个属性对应的归一化方式 一维向量
@@ -42,13 +41,13 @@ def normalize(arr, method, standard=None):
 
 
 # put datasets which the path ordered together, and ends with '.dataType'
-def putDatasTogether(path, dataType):
+def get_features_from_datasets(path, dataType):
     files = os.listdir(path)
-    featureSet = set()
+    featureDict = dict()
+    featureFormula = []
     for file in files:
         if not (file.endswith('.' + dataType) and file.startswith('drill')):
             files.pop(files.index(file))
-    files.pop(files.index('README.md'))
     with open(path + "/attributes.py", encoding="utf-8") as attr:
         existAttr = len(attr.readlines())
     for file in files:
@@ -57,16 +56,55 @@ def putDatasTogether(path, dataType):
             features = csvFile[0]
         for feature in features:
             feature = feature.split(' ')
+            featureDict[feature[0]] = '""\n'
             try:
-                feature[1] = "\"" + feature[1] + "\""
+                featureDict[feature[0]] = '"' + feature[1] + '"\n'
             except:
-                feature.append("\"\"")
+                pass
             finally:
-                feature[-1] = feature[-1] + '\n'
-                featureSet.add(' = '.join(feature))
-    featureSet = list(featureSet)
-    featureSet.pop(featureSet.index(' = ""\n'))
-    if len(featureSet) != existAttr:
+                pass
+
+    for ft in featureDict.items():
+        featureFormula.append(' = '.join(ft))
+    put_data_in_a_table(path, files)
+    if len(featureDict) != (existAttr + 1):
         with open(path + "/attributes.py", "wt", encoding="utf-8-sig") as attr:
-            attr.writelines(sorted(featureSet))
-    return sorted(featureSet)
+            attr.writelines(sorted(featureFormula))
+        put_data_in_a_table(path, files)
+    return featureDict
+
+
+def put_data_in_a_table(path, files):
+    attributesMatrix = []
+    attributesIndex = dict()
+    with open(path + "/attributes.py", encoding="utf-8-sig") as attrFile:
+        allAttr = attrFile.readlines()
+        emptyRow = ["" for i in range(len(allAttr))]
+        firstRow = emptyRow.copy()
+        for attrNo in range(len(allAttr)):
+            attrCode = allAttr[attrNo].split(" = ")[0]
+            attributesIndex[attrCode] = attrNo
+            firstRow[attrNo] = attrCode
+    attributesMatrix.append(firstRow)
+    for file in files:
+        currentAttrMap = dict()
+        with open(path + '/' + file, encoding="utf-8-sig") as f:
+            csvFile = list(csv.reader(f))
+            features = csvFile[0]
+            for feature in features:
+                featureCode = feature.split(' ')[0]
+                if featureCode in attributesIndex:
+                    currentAttrMap[featureCode] = (features.index(feature), attributesIndex[featureCode])
+            for row in range(1, len(csvFile)):
+                newRow = emptyRow.copy()
+                for column in range(len(csvFile[0])):
+                    currentFeature = features[column].split(' ')[0]
+                    if currentFeature:
+                        attrMap = currentAttrMap[currentFeature]
+                        newRow[attrMap[1]] = csvFile[row][column]
+                attributesMatrix.append(newRow)
+    with open(path + '/putTogether.csv', "w") as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerows(attributesMatrix)
+
+
--- machineLearning/reductionDim/PCA/pca_v1.0.py
+++ machineLearning/reductionDim/PCA/pca_v1.0.py
@@ -3,17 +3,15 @@ import matplotlib.pyplot as plt
 import csv
 
 import sys
+
 sys.path.append("../../")
+
 from dataPretreatment import normalized as nmlz
 
-class pca():
-	def __init__(self):
-		dataSetPwd = "../../dataSet/drillData_CSV"
-		self.dataType = 'csv'
-		self.allFeatures = nmlz.putDatasTogether(dataSetPwd, self.dataType)
-	def getinfo(self):
-		print(self.allFeatures)
-		print(len(self.allFeatures))
 
-conclusion = pca()
-conclusion.getinfo()
+class datas():
+    def __init__(self):
+        dataSetPwd = "../../dataSet/drillData_CSV"
+        self.dataType = 'csv'
+        self.allFeatures = nmlz.get_features_from_datasets(dataSetPwd, self.dataType)
+
